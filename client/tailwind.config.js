d512fd2d94a8dad0291747b5b6f49809c85d7588
Billy <osayebilly98@gmail.com>
2025-02-25 04:59:47 +0300

    initial setup of vite and tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // Scans all files in src folder with these extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
